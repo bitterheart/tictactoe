a2433cc89a0cbc60d3492bb2742c2a5f1c7e8060
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-01 22:37:34 -0400

    adding tests

---
+++ protractor_conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+    seleniumAddress: 'http://0.0.0.0:4444/wd/hub'
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Spec patterns are relative to the current working directly when
+  // protractor is called.
+  specs: ['test/e2e/*-spec.js'],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
---
+++ test/e2e/e2e-spec.js
@@ -0,0 +1,8 @@
+describe('homepage', function() {
+    beforeEach(function() {
+	browser.get('http://127.0.0.1:9000/');
+    });
+    it('should load the page', function() {
+	expect(element(by.css('h1'))).toEqual('Tic Tac Toe');
+    });
+});
