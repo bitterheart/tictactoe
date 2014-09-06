e50a6d2d332137568ec26bfeaad7f31b0d792b40
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 16:15:31 -0400

    java!=js

--- protractor_conf.js
+++ protractor_conf.js
@@ -1,18 +1,19 @@
 exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+	'browserName': 'chrome'
+    },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['test/e2e/*-spec.js'],
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['test/e2e/*-spec.js'],
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+	showColors: true,
+	defaultTimeoutInterval: 30000
+    },
+    baseUrl: 'http://127.0.0.1:9000/'
 };
