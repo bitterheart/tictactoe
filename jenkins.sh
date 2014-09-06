f4620b787688552c4b7feea1c13e09c4d9443b33
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 13:36:41 -0400

    firefox vs chrome

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ls
-ls test/e2e/*-spec.js
+yum provides *bin/chrome
+yum search chrome
+yum provides *bin/firefox
+yum search firefox
+which firefox
 ./node_modules/protractor/bin/webdriver-manager update --standalone
 grunt pro --no-color
--- protractor_conf.js
+++ protractor_conf.js
@@ -1,9 +1,9 @@
 exports.config = {
-    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    seleniumAddress: 'http://0.0.0.1:4444/wd/hub',
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   // Spec patterns are relative to the current working directly when
