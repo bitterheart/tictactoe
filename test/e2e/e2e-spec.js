f10f4706763ac2e3406e1dc113fb79a10a5df640
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 14:52:57 -0400

    server error

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -4,7 +4,7 @@
 	    browser.get('http://127.0.0.1:9000/');
 	});
 	it('should load the page', function() {
-	    expect(element(by.css('h1'))).toEqual('Tic Tac Toe');
+	    expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
 	});
     });
 })();
