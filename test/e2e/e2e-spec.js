aa0011690b9e05292cc152d727514c47bd9df73f
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 16:41:01 -0400

    not index.html

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,8 +1,5 @@
 describe('homepage', function() {
-    var ptor;
     beforeEach(function() {
-	ptor = protractor.getInstance();
-	ptor.ignoreSynchronization = true;
 	browser.get('http://127.0.0.1:9000/index.html');
     });
     rit('should load the page', function() {
