73b92fff1f03778378467e8befd0c81d5c7ed334
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 13:48:08 -0400

    let us install chrome

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 yum provides *bin/chrome
-yum search chrome
-yum provides *bin/firefox
-yum search firefox
-which firefox
+if [ ! -f google-chrome-stable_current_x86_64.rpm ]
+then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+fi
+sudo yum install -y google-chrome-stable_current_x86_64.rpm
+sudo yum install -y firefox
 ./node_modules/protractor/bin/webdriver-manager update --standalone
 grunt pro --no-color
