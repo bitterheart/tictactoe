952f5b82f62bb9dbb0576699ebbeaa0a43574ae9
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 16:18:34 -0400

    not index.html

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -4,7 +4,7 @@
 	beforeEach(function() {
 	    ptor = protractor.getInstance();
 	    ptor.ignoreSynchronization = true;
-	    browser.get('http://127.0.0.1:9000/index.html');
+	    browser.get('index.html');
 	});
 	rit('should load the page', function() {
 	    expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
