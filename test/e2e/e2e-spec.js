87315ad0f1cfd583ae80e01a41acbefd38b26a93
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 17:11:15 -0400

    wtf

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,8 +1,6 @@
-describe('homepageafdsfads', function() {
-    beforeEach(function() {
-	browser.get('index.html');
-    });
+describe('homepage should load', function() {
     it('should load the page', function() {
+	browser.get('index.html');
 	expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
     });
 });
