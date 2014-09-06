ebd107db5a245471078586af237dbae8b901074b
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 18:59:24 -0400

    phantomjs

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,7 +1,9 @@
 describe('homepage should load', function() {
+    beforeEach(function(){
+	browser.get('http://127.0.0.1:9000/index.html');
+    });
     it('should load the page', function() {
 	console.log('a');
-	browser.get('http://127.0.0.1:9000/index.html');
 	console.log('b');
 	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
 	console.log('c');
