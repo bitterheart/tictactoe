1044283e8dea97415e62fcf0e9fbcabfec26080a
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 16:39:08 -0400

    not index.html

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,13 +1,11 @@
-(function(){
+describe('homepage', function() {
     var ptor;
-    describe('homepage', function() {
-	beforeEach(function() {
-	    ptor = protractor.getInstance();
-	    ptor.ignoreSynchronization = true;
-	    browser.get('http://127.0.0.1:9000/index.html');
-	});
-	rit('should load the page', function() {
-	    expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
-	});
+    beforeEach(function() {
+	ptor = protractor.getInstance();
+	ptor.ignoreSynchronization = true;
+	browser.get('http://127.0.0.1:9000/index.html');
     });
-})();
+    rit('should load the page', function() {
+	expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
+    });
+});
