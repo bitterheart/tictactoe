1d2ffa91c3130b4e01c27e1e0e1dab0ba4ee1e7f
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 16:11:42 -0400

    java!=js

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,6 +1,9 @@
 (function(){
+    var ptor;
     describe('homepage', function() {
 	beforeEach(function() {
+	    ptor = protractor.getInstance();
+	    ptor.ignoreSynchronization = true;
 	    browser.get('http://127.0.0.1:9000/');
 	});
 	rit('should load the page', function() {
