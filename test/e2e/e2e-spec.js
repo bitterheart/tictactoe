e3a4e7c63ef4c0ab357b094d3e190cb1d63e9aae
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 15:01:14 -0400

    rit

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -3,7 +3,7 @@
 	beforeEach(function() {
 	    browser.get('http://127.0.0.1:9000/');
 	});
-	it('should load the page', function() {
+	rit('should load the page', function() {
 	    expect(element(by.css('h1'))).toEqual('WTic Tac Toe');
 	});
     });
