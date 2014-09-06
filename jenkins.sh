2e4671daf22a338c70c0d24e580be67bf4c35f4c
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 14:23:34 -0400

    transfering to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-npm install express --save-dev
-npm install grunt-express --save-dev
-more package.json
+sudo yum install -y wget
+if [ ! -f epel-release-7-1.noarch.rpm ]
+then
+    wget http://mirror.umd.edu/fedora/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
+fi
+sudo yum install -y epel-release-7-1.noarch.rpm || true
+sudo yum update -y
+sudo yum -y install nodejs-grunt-cli npm
+npm install
 if [ ! -f google-chrome-stable_current_x86_64.rpm ]
 then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
