ed7a8a39bd23d87799f34465d263a03e289a543e
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 19:39:55 -0400

    phantomjs

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -3,6 +3,9 @@ describe('homepage should load', function() {
 	browser.driver.get('http://127.0.0.1:9000/index.html');
     });
     it('should load the page', function() {
-	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
+	element(by.css('h1')).then(function(title){
+	    expect(title.getText()).toBe('wrong');
+	});
+//	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
     });
 });
