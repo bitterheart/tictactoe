e2a180aea224812a6a35603003b080f96fe397d0
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 21:00:51 -0400

    wtf

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,6 +1,6 @@
 describe('homepage should load', function() {
     beforeEach(function(){
-	browser.driver.get('index.html');
+	browser.driver.get('http://127.0.0.1:9000/index.html');
     });
     it('should load the page', function() {
 	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
