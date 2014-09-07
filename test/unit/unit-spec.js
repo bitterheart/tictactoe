28b35d26262acbd297b16894db4d48c95d6a72a4
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 17:26:03 -0400

    new epel

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -1,13 +1,19 @@
 "use strict";
-describe('controller unit test',function(){
-    it('initially',function(){
-	var $scope={};
-	var ctrl=$controller('controller',{$scope:$scope});
-	expect($scope.grid).toEqual([[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}],[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}],[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}]]);
-    });
-    it('make a move',function(){
-	var $scope={};
-	var ctrl=$controller('controller',{$scope:$scope});
-	ctrl.pickMe($scope.grid[0,0]);
+define(['$controller'],function(){
+    describe('controller unit test',function(){
+	var $controller;
+	beforeEach(inject(function(controller){
+	    $controller=controller;
+	}));
+	it('initially',function(){
+	    var $scope={};
+	    var ctrl=$controller('controller',{$scope:$scope});
+	    expect($scope.grid).toEqual([[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}],[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}],[{value:'-',status:'player'},{value:'-',status:'player'},{value:'-',status:'player'}]]);
+	});
+	it('make a move',function(){
+	    var $scope={};
+	    var ctrl=$controller('controller',{$scope:$scope});
+	    ctrl.pickMe($scope.grid[0,0]);
+	});
     });
 });
