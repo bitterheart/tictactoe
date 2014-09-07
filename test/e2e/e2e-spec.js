2b3e76feeab509fbf365a4510d0461df7afe34d3
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 22:10:40 -0400

    adding reports

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,8 +1,11 @@
-describe('homepage should load', function() {
+describe('tic tac toe', function() {
     beforeEach(function(){
 	browser.driver.get('http://127.0.0.1:9000/index.html');
     });
-    it('should load the page', function() {
+    it('should have a title', function(){
 	expect(element(by.css('h1')).getText()).toEqual('Tic Tac Toe');
+    });
+    it('should have a grid',function(){
+	expect(element(by.css('div.grid'))).toBeDefined();
     });
 });
