d25c03b7c55814f8e5e4747bdeb4f850aa6f85e4
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 11:49:40 -0400

    run the tests

--- src/scripts/directives.js
+++ src/scripts/directives.js
@@ -3,7 +3,7 @@
     app.directive('grid',function(){
 	return{
 	    restrict:'C',
-	    template: 'fadsfdsfds'
+	    template: '<div class="row"></div><div class="row"></div><div class="row"></div>'
 	};
     });
 })();
--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -1,16 +1,14 @@
 "use strict";
 describe('grid directive',function(){
     var $compile, $rootScope;
-
     beforeEach(module('app'));
     beforeEach(inject(['$compile','$rootScope', function($c, $r) {
 	$compile = $c;
 	$rootScope = $r;
     }]
 		     ));
-
     it("should display the welcome text properly", function() {
-	var element = $compile('<div class="grid">User</div>')($rootScope);
-	expect(element.html()).toEqual('wrong');
+	var element = $compile('<div class="grid"></div>')($rootScope);
+	expect(element.html()).toEqual('<div class="row"></div><div class="row"></div><div class="row"></div>');
     })
 });
