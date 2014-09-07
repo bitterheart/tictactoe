b87a24d8b96d03e41dff9573a85c6bcb50e7ebc8
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 11:11:07 -0400

    new epel

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -1,13 +1,16 @@
 "use strict";
 describe('grid directive',function(){
     var element;
+    var scope;
     beforeEach(module('app'));
     beforeEach(inject(function($compile,$rootScope){
 	var element=angular.element('<div class="grid"></div>');
-	$compile(element)($rootScope);
-	$rootScope.digest();
+	scope=$rootScope;
+	scope.defined=false;
+	$compile(element)(scope);
+	scope.digest();
     }));
     it('correct content',function(){
-	expect(element.html()).toEqual('<div class="row">');
+	expect(element.scope().html()).toEqual('<div class="row">');
     });
 });
