2cf91b3726adcbcbd70e1275548a000d5ff9cf2d
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 12:57:01 -0400

    run the tests

--- src/scripts/directives.js
+++ src/scripts/directives.js
@@ -5,9 +5,9 @@
 	    restrict:'C',
 	    link: function(scope,element,attrs){
 		for(var i=0;i<3;i++){
-		    var row=angular.element(document.createElement('<div>')).addClass('row');
+		    var row=angular.element(document.createElement('div')).addClass('row');
 		    for(var j=0;j<3;j++){
-			var col=angular.element(document.createElement('<div>')).addClass('col');
+			var col=angular.element(document.createElement('div')).addClass('col');
 			row.appendChild(col);
 		    }
 		    element.appendChild(row);
