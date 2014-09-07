7fbe09543caf34e8bcb1f3a1530ee220c9ada096
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-06 20:07:48 -0400

    phantomjs

--- protractor_conf.js
+++ protractor_conf.js
@@ -3,7 +3,7 @@ exports.config = {
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-	'browserName': 'chrome',
+	'browserName': 'firefox',
     },
 /*
     capabilities: {
--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -3,9 +3,6 @@ describe('homepage should load', function() {
 	browser.driver.get('http://127.0.0.1:9000/index.html');
     });
     it('should load the page', function() {
-	element(by.css('h1')).then(function(title){
-	    expect(title.getText()).toBe('wrong');
-	});
-//	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
+	expect(element(by.css('h1')).getText()).toEqual('WTic Tac Toe');
     });
 });
