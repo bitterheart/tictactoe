e122686334cb1a63064c66e3fdea29d96d8112aa
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 08:34:46 -0400

    run the tests

--- unit-test.sh
+++ unit-test.sh
@@ -4,6 +4,17 @@ if [ -f epel-release-7-1.noarch.rpm ]
 then
     wget http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
 fi
+if [ ! -f google-chrome-stable_current_x86_64.rpm ]
+then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+fi
+sudo yum install -y google-chrome-stable_current_x86_64.rpm
+sudo yum install -y Xvfb
+if [ ! -d /tmp/.X99-lock ]
+then
+    Xvfb :99 -ac -screen 0 1024x768x24 &
+    export DISPLAY=:99
+fi
 npm install --save-dev karma
 npm install karma-jasmine --save-dev
 npm install karma-chrome-launcher --save-dev
