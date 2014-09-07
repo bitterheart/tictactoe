97a6337c873e484162b0428965fb409ec3ddad03
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 18:35:41 -0400

    unit testing

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -12,5 +12,17 @@ describe('tic tac toe',function(){
 	    expect($rootScope.grid[1].length).toEqual(3);
 	    expect($rootScope.grid[2].length).toEqual(3);
 	}));
+	it('initially everything should be status -',inject(function($rootScope,$controller){
+	    $controller('controller',{$scope:$rootScope});
+	    expect($rootScope.grid[0][0].value).toEqual('sdafads');
+	    expect($rootScope.grid[0][1].value).toEqual('-');
+	    expect($rootScope.grid[0][2].value).toEqual('-');
+	    expect($rootScope.grid[1][0].value).toEqual('-');
+	    expect($rootScope.grid[1][1].value).toEqual('-');
+	    expect($rootScope.grid[1][2].value).toEqual('-');
+	    expect($rootScope.grid[2][0].value).toEqual('-');
+	    expect($rootScope.grid[2][1].value).toEqual('-');
+	    expect($rootScope.grid[2][2].value).toEqual('-');
+	}));
     });
 });
