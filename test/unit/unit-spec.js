40e809d72856acd90bad4f2a981e32cb3fc93375
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 18:27:39 -0400

    unit testing

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -3,6 +3,7 @@ describe('tic tac toe',function(){
     describe('controller',function(){
 	beforeEach(module('app'));
 	it('initially',inject(function($rootScope,$controller){
+	    $controller('controller',{$scope:$rootScope});
 	    expect($rootScope.grid.length).toEqual(3);
 	}));
     });
