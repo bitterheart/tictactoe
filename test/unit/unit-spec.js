71de20a16fff415a252d58098f23864af4bf93e9
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 18:48:57 -0400

    unit testing

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -1,7 +1,22 @@
 "use strict";
 describe('tic tac toe',function(){
     describe('controller',function(){
-	beforeEach(module('app'));
+	beforeEach(function(){
+	    var mockTickTackToeService={};
+	    module('app',function($provide){
+		$provide.value('ticTacToeService',mockTickTackToeService);
+	    });
+	    inject(function($q){
+		mockTickTacToeService.decide=function(){
+		    var defer=$q.defer();
+		    defer.resolve({winners:{},free:1});
+		    return defer.promise;
+		};
+		mockTickTacToeService.pick=function(){
+		    return {};
+		};
+	    });
+	}));
 	it('initially it should have a grid with three rows',inject(function($rootScope,$controller){
 	    $controller('controller',{$scope:$rootScope});
 	    expect($rootScope.grid.length).toEqual(3);
@@ -26,6 +41,28 @@ describe('tic tac toe',function(){
 	}));
 	it('initially everything should be status player',inject(function($rootScope,$controller){
 	    $controller('controller',{$scope:$rootScope});
+	    expect($rootScope.grid[0][0].status).toEqual('player');
+	    expect($rootScope.grid[0][1].status).toEqual('player');
+	    expect($rootScope.grid[0][2].status).toEqual('player');
+	    expect($rootScope.grid[1][0].status).toEqual('player');
+	    expect($rootScope.grid[1][1].status).toEqual('player');
+	    expect($rootScope.grid[1][2].status).toEqual('player');
+	    expect($rootScope.grid[2][0].status).toEqual('player');
+	    expect($rootScope.grid[2][1].status).toEqual('player');
+	    expect($rootScope.grid[2][2].status).toEqual('player');
+	}));
+	it('i make a move (center square) it should be xed, and the computer make a move',inject(function($rootScope,$controller,ticTacToeService){
+	    $controller('controller',{$scope:$rootScope});
+	    $rootScope.pickMe($rootScope.grid[1][1]);
+	    expect($rootScope.grid[0][0].value).toEqual('-');
+	    expect($rootScope.grid[0][1].value).toEqual('-');
+	    expect($rootScope.grid[0][2].value).toEqual('-');
+	    expect($rootScope.grid[1][0].value).toEqual('-');
+	    expect($rootScope.grid[1][1].value).toEqual('X');
+	    expect($rootScope.grid[1][2].value).toEqual('-');
+	    expect($rootScope.grid[2][0].value).toEqual('-');
+	    expect($rootScope.grid[2][1].value).toEqual('-');
+	    expect($rootScope.grid[2][2].value).toEqual('-');
 	    expect($rootScope.grid[0][0].status).toEqual('player');
 	    expect($rootScope.grid[0][1].status).toEqual('player');
 	    expect($rootScope.grid[0][2].status).toEqual('player');
