7347a1f7d7783c93160682322c607ec64c4b58c8
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 18:25:41 -0400

    unit testing

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -2,7 +2,7 @@
 describe('tic tac toe',function(){
     describe('controller',function(){
 	beforeEach(module('app'));
-	it('initially',inject(function($controller,$rootScope){
+	it('initially',inject(function($rootScope,$controller){
 	    expect($rootScope.grid.length).toEqual(3);
 	}));
     });
