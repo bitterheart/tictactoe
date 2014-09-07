513e9f1808244cd7bb25bf637a01be45855daaa6
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 08:24:50 -0400

    run the tests

--- unit-test.sh
+++ unit-test.sh
@@ -5,5 +5,5 @@ then
     wget http://mirror.sfo12.us.leaseweb.net/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
 fi
 npm install --save-dev karma
-npm install karma-ng-html2js-preprocessor --save-dev
+# npm install karma-ng-html2js-preprocessor --save-dev
 ./node_modules/karma/bin/karma start karma.conf.js
