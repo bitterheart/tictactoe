2a5b6ab0ebc426d682faf53619330159fe185cd0
Emory Merryman <Yc6hR2VEpBdiFT@localhost.localdomain>
2014-09-07 20:32:19 -0400

    unit testing

--- test/unit/unit-spec.js
+++ test/unit/unit-spec.js
@@ -69,7 +69,7 @@ describe('tic tac toe',function(){
 	    expect($rootScope.grid[1][2].value).toEqual('-');
 	    expect($rootScope.grid[2][0].value).toEqual('-');
 	    expect($rootScope.grid[2][1].value).toEqual('-');
-	    expect($rootScope.grid[2][2].value).toEqual('O');
+	    expect($rootScope.grid[2][2].value).toEqual('-');
 	    expect($rootScope.grid[0][0].status).toEqual('player');
 	    expect($rootScope.grid[0][1].status).toEqual('player');
 	    expect($rootScope.grid[0][2].status).toEqual('player');
