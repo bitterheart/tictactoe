15e643ea65e21b1322da30849728f3a082c1ae65
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-14 02:02:15 +0000

    working on tests

--- test/unit/ticTacToeService-spec.js
+++ test/unit/ticTacToeService-spec.js
@@ -7,11 +7,6 @@ describe('ticTacToeService', function() {
     ]));
     beforeEach(inject(['$q', 'ticTacToeService',
         function($q, _ticTacToeService_) {
-            for (var key in $provide) {
-                console.log('key ' + key);
-            }
-            console.log('beforeEach == ' + $provide);
-            console.log('beforeEach == ' + _ticTacToeService_);
             $provide.provider('randomService', function() {
                 this.$get=angular.noop;
                 this.random = function() {
@@ -23,9 +18,6 @@ describe('ticTacToeService', function() {
             ticTacToeService = _ticTacToeService_;
         }
     ]));
-    it('a', function() {
-        expect(1).toEqual(2);
-    });
     it('b', function() {
         var data = [{
             value: '-'
@@ -34,7 +26,6 @@ describe('ticTacToeService', function() {
         }, {
             value: '1'
         }];
-        console.log('it b ' + ticTacToeService);
         ticTacToeService.pick(data).then(function(result) {
             expect(10).toEqual(20);
         });
