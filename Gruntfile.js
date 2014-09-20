12ae5c506184764a0e2917421cf11c016a063927
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-20 21:54:28 +0000

    e2e configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,70 +11,70 @@ module.exports = function(grunt) {
 					stdout: true
 				},
 				command: require('path').resolve(__dirname, 'node_modules', 'protractor', 'bin', 'webdriver-manager') + ' update'
-			},
-			selenium_webdriver_phantom: {
-				chrome: {
+			}
+		},
+		selenium_webdriver_phantom: {
+			chrome: {
 
-				},
-				phantom: {
-					options: {
-						phantom: {}
-					}
-				},
-				others: {
-
-				}
 			},
-
-			protractor_webdriver: {
-				ci: {
-					options: {
-						path: 'node_modules/protractor/bin/',
-						command: 'webdriver-manager start'
-					}
-				}
-			},
-			protractor: {
+			phantom: {
 				options: {
-					//				configFile: "node_modules/protractor/docs/referenceConf.js", // Default config file
-					//				keepAlive: false, // If false, the grunt process stops when the test fails.
-					noColor: false, // If true, protractor will not use colors in its output.
-					args: {
-						// Arguments passed to the command
-					}
-				},
-				ci: {
-					options: {
-						configFile: "config/protractor.conf.js", // Target-specific config file
-						keepAlive: false,
-						args: {} // Target-specific arguments
-					}
+					phantom: {}
 				}
 			},
-			express: {
-				server: {
-					options: {
-						hostname: '127.0.0.1',
-						port: 9000,
-						bases: ['src']
-					},
+			others: {
+
+			}
+		},
+
+		protractor_webdriver: {
+			ci: {
+				options: {
+					path: 'node_modules/protractor/bin/',
+					command: 'webdriver-manager start'
+				}
+			}
+		},
+		protractor: {
+			options: {
+				//				configFile: "node_modules/protractor/docs/referenceConf.js", // Default config file
+				//				keepAlive: false, // If false, the grunt process stops when the test fails.
+				noColor: false, // If true, protractor will not use colors in its output.
+				args: {
+					// Arguments passed to the command
+				}
+			},
+			ci: {
+				options: {
+					configFile: "config/protractor.conf.js", // Target-specific config file
+					keepAlive: false,
+					args: {} // Target-specific arguments
+				}
+			}
+		},
+		express: {
+			server: {
+				options: {
+					hostname: '127.0.0.1',
+					port: 9000,
+					bases: ['src']
 				},
 			},
-			karma: {
-				unit: {
-					configFile: 'config/karma.conf.js',
-					port: 9879,
-					singleRun: false,
-					autoWatch: true,
-					reporters: ['progress'],
-				},
-				continuous: {
-					configFile: 'config/karma.conf.js',
-					port: 9880,
-					singleRun: true,
-					autoWatch: false,
-					reporters: ['html', 'coverage'],
-				}
+		},
+		karma: {
+			unit: {
+				configFile: 'config/karma.conf.js',
+				port: 9879,
+				singleRun: false,
+				autoWatch: true,
+				reporters: ['progress'],
+			},
+			continuous: {
+				configFile: 'config/karma.conf.js',
+				port: 9880,
+				singleRun: true,
+				autoWatch: false,
+				reporters: ['html', 'coverage'],
 			}
 		}
 	});
