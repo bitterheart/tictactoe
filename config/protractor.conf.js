7dd0191b9bfbfef4d19cb71de394d484ee8cf1b0
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-21 13:42:08 +0000

    use chrome for e2e

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,35 +1,34 @@
 var HtmlReporter = require('protractor-html-screenshot-reporter');
 exports.config = {
-    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+        seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
-    // Capabilities to be passed to the webdriver instance.
-    //    capabilities: {
-    //	'browserName': 'pPantomJS',
-    //    },
+        // Capabilities to be passed to the webdriver instance.
+        //    capabilities: {
+        //	'browserName': 'pPantomJS',
+        //    },
 
-    capabilities: {
-        'browserName': 'chrome',
-        'binary':'google-chrome'
-    },
+        capabilities: {
+            'browserName': 'Chrome',
+            'ChromeOptions': {},
 
 
-    // Spec patterns are relative to the current working directly when
-    // protractor is called.
-    specs: ['../test/e2e/*-spec.js'],
+            // Spec patterns are relative to the current working directly when
+            // protractor is called.
+            specs: ['../test/e2e/*-spec.js'],
 
-    // Options to be passed to Jasmine-node.
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000
-    },
-    baseUrl: 'http://127.0.0.1:9000/',
-    onPrepare: function() {
-        // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
-        jasmine.getEnv().addReporter(new HtmlReporter({
-            baseDirectory: '/tmp/screenshots',
-            takeScreenShotsOnlyForFailedSpecs: true,
-            docTitle: 'protractor reporter'
-        }));
-        browser.driver.manage().window().setSize(1600, 800);
-    }
-};
+            // Options to be passed to Jasmine-node.
+            jasmineNodeOpts: {
+                showColors: true,
+                defaultTimeoutInterval: 30000
+            },
+            baseUrl: 'http://127.0.0.1:9000/',
+            onPrepare: function() {
+                // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
+                jasmine.getEnv().addReporter(new HtmlReporter({
+                    baseDirectory: '/tmp/screenshots',
+                    takeScreenShotsOnlyForFailedSpecs: true,
+                    docTitle: 'protractor reporter'
+                }));
+                browser.driver.manage().window().setSize(1600, 800);
+            }
+        };
