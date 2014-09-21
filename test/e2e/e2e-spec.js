dc970fa86687b73f24550095346a46c8ce0a4699
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-21 16:56:35 +0000

    use chrome for e2e

--- test/e2e/e2e-spec.js
+++ test/e2e/e2e-spec.js
@@ -1,7 +1,7 @@
-"strict;"
+"strict";
 describe('tic tac toe', function() {
 	beforeEach(function() {
-		browser.get('http://127.0.0.1:9000/index.html');
+		browser.driver.get('http://127.0.0.1:9000/index.html');
 	});
 	it('should have a title', function() {
 		expect(element(by.css('h1')).getText()).toEqual('Tic Tac Toe');
