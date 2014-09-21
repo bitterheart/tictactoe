82a518fde5e2cd592092dfc0de17f46eef3604e9
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-21 17:50:16 +0000

    use chrome for e2e

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -27,10 +27,12 @@ exports.config = {
     baseUrl: 'http://127.0.0.1:9000/',
     onPrepare: function() {
         // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
-           jasmine.getEnv().addReporter(new HtmlReporter({
-                   baseDirectory: '/tmp/screenshots',
-                   takeScreenShotsOnlyForFailedSpecs: true,
-                   docTitle: 'protractor reporter'
-               }));
+        /*
+        jasmine.getEnv().addReporter(new HtmlReporter({
+                           baseDirectory: '/tmp/screenshots',
+                           takeScreenShotsOnlyForFailedSpecs: true,
+                           docTitle: 'protractor reporter'
+                       }));
+                       */
     }
 };
